5836c4224c3d55cf394ea8a842e331a2aa6bf330
trippymajo <elyadesu@yandex.ru>
2023-06-28 18:37:28 +0300

    C# solution for 6 kyu Decode the Morse

---
+++ CodeWars/6 kyu Decode the Morse code.cs
@@ -0,0 +1,19 @@
+﻿class MorseCodeDecoder
+{
+	public static string Decode(string morseCode)
+	{
+		string retVal = null, morseWord = null;
+		morseCode.Trim();
+		string[] words = morseCode.Split(" ");
+		foreach (string letter in words)
+		{
+			if (letter == "")
+			{
+				retVal += " ";
+			}
+			retVal += MorseCode.Get(letter);
+		}
+		retVal = string.Join(" ", retVal.Split(new char[] { ' ' }, System.StringSplitOptions.RemoveEmptyEntries));
+		return retVal;
+	}
+}
