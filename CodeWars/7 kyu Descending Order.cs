c2a7f64a929e0a303d981bc8a7e0b302947df6d2
trippymajo <elyadesu@yandex.ru>
2023-07-09 14:26:34 +0300

    .Net solution 7 kyu Descending Order

---
+++ CodeWars/7 kyu Descending Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+
+public static class Kata
+{
+	public static int DescendingOrder(int num)
+	{
+		ArrayList arrList = new ArrayList();
+
+		while (num != 0)
+		{
+			arrList.Add(num % 10);
+			num /= 10;
+		}
+
+		arrList.Sort();
+		arrList.Reverse();
+
+		foreach (int j in arrList)
+		{
+			num = num * 10 + j;
+		}
+		return num;
+	}
+}
